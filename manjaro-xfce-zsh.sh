70c7a9432f2a5ab91d9c6f64eb834ffaa8712fd2
Filip Havrlent <filip@havrlent.com>
2021-10-07 22:04:34 +0200

    chore: debug

--- manjaro-xfce-zsh.sh
+++ manjaro-xfce-zsh.sh
@@ -8,8 +8,7 @@ git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zs
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+source .zshrc
 autoload -U compinit && compinit
 chsh -s $(which zsh)
 ##############
-
-echo "source .zshrc"

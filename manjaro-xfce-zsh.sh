2f4f491b5bdc0a9bff41b16c5d065b86fdff85fd
Filip Havrlent <filip@havrlent.com>
2021-10-07 20:54:37 +0200

    chore: :bug: source does not work from script, add echo for ultimate copy paste experience

--- manjaro-xfce-zsh.sh
+++ manjaro-xfce-zsh.sh
@@ -6,7 +6,8 @@ git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zs
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-source .zshrc
 autoload -U compinit && compinit
 chsh -s $(which zsh)
 ##############
+
+echo "source .zshrc"
